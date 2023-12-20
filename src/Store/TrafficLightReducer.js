accdfdf4c6ea4d6dc5bcfda9eb40932045a89ed0
Aleksandr Leimberg <aleimberg@west.local>
2023-12-20 10:59:05 +0000

    a few changes

--- src/Store/TrafficLightReducer.js
+++ src/Store/TrafficLightReducer.js
@@ -1,16 +1,5 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
 
-export const fetchDataWithDelay = createAsyncThunk(
-    'data/fetchWithDelay',
-    async () => {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                const data = ['apple', 'banana', 'orange'];
-                resolve(data);
-            }, 10000); // Simulating a 2-second delay
-        });
-    }
-);
 
 
 const trafficLightSlice = createSlice({
@@ -20,25 +9,13 @@ const trafficLightSlice = createSlice({
         changeLight: (state) => {
             state.currentLight = state.currentLight === 'red' ? 'yellow' : state.currentLight === 'yellow' ? 'green' : 'red';
         },
-        extraReducers: (builder) => {
-            builder
-                .addCase(fetchDataWithDelay.pending, (state) => {
-                    state.loading = true;
-                    state.error = null;
-                })
-                .addCase(fetchDataWithDelay.fulfilled, (state, action) => {
-                    state.loading = false;
-                    state.items = action.payload;
-                })
-                .addCase(fetchDataWithDelay.rejected, (state, action) => {
-                    state.loading = false;
-                    state.error = action.error.message;
-                });
-        },
+        changeLoadingStatus: (state) => {
+            state.loading = !state.loading
+        }
     },
 });
 
-export const {changeLight} = trafficLightSlice.actions;
+export const {changeLight, changeLoadingStatus} = trafficLightSlice.actions;
 
 export default trafficLightSlice.reducer;
 
--- src/TrafficLight.js
+++ src/TrafficLight.js
@@ -1,7 +1,7 @@
 // TrafficLight.js
 import React from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {changeLight, fetchDataWithDelay} from './Store/TrafficLightReducer';
+import {changeLight, changeLoadingStatus} from './Store/TrafficLightReducer';
 import s from './TrafficLight.module.css'
 
 const TrafficLight = () => {
@@ -9,9 +9,8 @@ const TrafficLight = () => {
     const currentLight = useSelector((state) => state.trafficLight.currentLight);
     const statusLoading = useSelector((state) => state.trafficLight.loading);
 
-    const handleFetchData = () => {
-        //dispatch(fetchDataWithDelay());
-    };
+
+
 
 
     return (
@@ -22,7 +21,7 @@ const TrafficLight = () => {
             </div>
             <div className={s.section}>
                 <div>{statusLoading === true ? 'Loading ...' : null}</div>
-                <button onClick={handleFetchData} disabled={loading}>button</button>
+                <button onClick={() => dispatch(changeLoadingStatus())}>button</button>
             </div>
             <div className={s.section}>
                 <div>text</div>
